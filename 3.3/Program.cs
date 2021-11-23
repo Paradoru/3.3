a06881490b1c5b3c17fabb54508949f067578e3d
Paradoru <LicLexa@yandex.ru>
2021-11-23 23:33:48 +0200

    Добавьте файлы проекта.

---
+++ 3.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите x:");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите количество повторений:");
+                int n = int.Parse(Console.ReadLine());
+                double S = 0;
+                double b = 0;
+                double z = 1;
+                long F = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 2; j <= z; j++) F *= j;
+                    S = Math.Pow(x, z) / F;
+                    z += 4;
+                    b += S;
+                }
+                Console.WriteLine("b={0:F2}", b);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);  
+            }
+        }
+    }
+}
